59a70389a52a508b30e063a16905dfe2491f5e3e
dario <darioasaro29@gmail.com>
2020-02-03 12:23:40 -0300

    zombies sacan vida

--- recursos-ciudad-zombie/js/juego.js
+++ recursos-ciudad-zombie/js/juego.js
@@ -216,9 +216,11 @@ se ven las colisiones con los obstaculos. En este caso sera con los zombies. */
 Juego.calcularAtaques = function() {
   this.enemigos.forEach(function(enemigo) {
     if (this.intersecan(enemigo, this.jugador, this.jugador.x, this.jugador.y)) {
+      enemigo.comenzarAtaque(this.jugador)
       /* Si el enemigo colisiona debe empezar su ataque
       COMPLETAR */
     } else {
+      enemigo.dejarDeAtacar()
       /* Sino, debe dejar de atacar
       COMPLETAR */
     }
